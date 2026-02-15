4ec35ed7ced7ca06766f2f997e0d3bd36c8f7f7d
mulatta <67085791+mulatta@users.noreply.github.com>
2026-02-15 16:30:23 +0900

    feat: export snippet models from package

--- semanticscholar/__init__.py
+++ semanticscholar/__init__.py
@@ -2,3 +2,5 @@ from .AsyncSemanticScholar import AsyncSemanticScholar as AsyncSemanticScholar
 from .Dataset import Dataset as Dataset
 from .Release import Release as Release
 from .SemanticScholar import SemanticScholar as SemanticScholar
+from .SnippetSearchResult import Snippet as Snippet
+from .SnippetSearchResult import SnippetSearchResult as SnippetSearchResult
